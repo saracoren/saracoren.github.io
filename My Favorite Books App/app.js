8894a027505325c295860aade15d941ffd46f19c
Sara Coren <saracoren@Saras-MacBook-Air.local>
2019-04-05 14:38:19 -0400

    added a for loop and console logged isolated title names

--- My Favorite Books App/app.js
+++ My Favorite Books App/app.js
@@ -4,13 +4,14 @@ $(() => {
     // GET https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines&key=yourAPIKey
 
 const getData = () => {$.ajax({
-    url: "https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines&key=AIzaSyD4fQutk55ce3kvbO8in6uPvpLpWwVieOM",
+    url: "https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyD4fQutk55ce3kvbO8in6uPvpLpWwVieOM",
     type: "GET",
     data: {
-        totalItems : 1
+        items: {}
     }
 }).then(function(data) {
-    console.log(data)
+    for (let i = 0; i < 10; i++)
+    console.log(data.items[i].volumeInfo.title)
 })
 }
  getData()
